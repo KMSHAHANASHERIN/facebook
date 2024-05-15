46e3fac190b3e1eaebddb42948809ff36b4bc4d1
SHAHANA SHERIN KM <kmshahanasherin@gmail.com>
2024-05-15 19:02:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+       rules:{
+        fname:{
+            required:true,
+            minlength:4,
+            maxlength:10
+        },
+        sname:{
+            required:true,
+            minlength:4,
+            maxlength:10
+        },
+        emailAdress:{
+            required:true,
+            email:true
+        },
+        password:{
+            minlength:6
+        },
+        day:{
+            required:true
+        },
+        month:{
+            required:true
+        },
+        year:{
+            required:true
+        },
+        gender:{
+            required:true
+        }       
+
+       },
+       messages:{
+        fname:{
+            required:"enter first name",
+            minlength:"enter min chara"
+        }
+       }
+    })
+})
